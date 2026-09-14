28e43ccdf2da8f37121fed5b61629b8367fda04e
“Raul” <raulmj83@gmail.com>
2026-09-14 18:13:29 +0200

    change the button behaviour to fly

--- golem_first_jam/entities/chicken/chicken.gd
+++ golem_first_jam/entities/chicken/chicken.gd
@@ -5,12 +5,15 @@ enum Action {NONE, PECKING, FLYING, LAYING_EGG}
 const SPRITE_SCALE = 1
 
 @export var move_speed: int = 200 # px/s
+@export var pushes_to_fly: int = 3 # How many times you have to push during the quick press timer to fly (see time_to_quick_push)
+@export var time_to_quick_push = 0.5 # Max time you have to quick press in order to fly (s)
 @export var time_to_lay_egg: float = 3.0 # s
 @export var jump_max_impulse: float = 5.0 # Vertical impulse for flying (px/s)
 @export var gravity_factor: float = 0.02 # Gravity modifier, the higher the more gravity
 
 @onready var sprite: AnimatedSprite2D = $Sprite
-@onready var button_timer: Timer = $ButtonTimer
+@onready var hold_button_timer: Timer = $HoldButtonTimer
+@onready var quick_press_button_timer: Timer = $QuickPressButtonTimer
 
 var action: Action
 var button_charge: float # When you push repeatedly the button, this float "recharges", allowing you to fly when a threshold is surpassed
@@ -19,6 +22,7 @@ var z_offset: float # Distance from the floor when flying
 
 func _ready() -> void:
 	action = Action.NONE
+	quick_press_button_timer.wait_time = time_to_quick_push
 
 # Function to move in 8 directions
 func _physics_process(delta: float) -> void:
@@ -65,21 +69,25 @@ func manage_actions_input(delta: float):
 	if is_flying():	
 		return
 	
-	button_charge = clampf(button_charge - delta, 0, 10)	
+	# button_charge = clampf(button_charge - delta, 0, 10)	
 	if Input.is_action_just_pressed("a_button"):
-		button_timer.start(time_to_lay_egg)
-		button_charge += 0.5
-		if button_charge > 1.0:			
+		hold_button_timer.start(time_to_lay_egg)
+		if quick_press_button_timer.is_stopped():
+			quick_press_button_timer.start()
+		else:
+			button_charge += 1
+		if button_charge >= pushes_to_fly:			
 			fly()
 			button_charge = 0.0
-			button_timer.stop()
+			quick_press_button_timer.stop()
+			hold_button_timer.stop()
 		else:
 			peck()	
 	elif Input.is_action_just_released("a_button"):
 		action = Action.NONE
-		button_timer.stop()
+		hold_button_timer.stop()
 		
-	if not button_timer.is_stopped() and button_timer.time_left < time_to_lay_egg / 2.0:
+	if not hold_button_timer.is_stopped() and hold_button_timer.time_left < time_to_lay_egg / 2.0:
 		if not action == Action.LAYING_EGG:
 			action = Action.LAYING_EGG
 
@@ -111,7 +119,8 @@ func peck() -> void:
 	
 func fly() -> void:
 	action = Action.FLYING
-	z_velocity = -lerp(0.1, jump_max_impulse, button_charge - 0.3)
+	var impulse_factor = clampf((quick_press_button_timer.time_left / quick_press_button_timer.wait_time) * 8.0, 0, 1.0)
+	z_velocity = -lerp(0.2, jump_max_impulse, impulse_factor)
 	SignalBus.chicken_flies.emit()
 	
 func lay_egg() -> void:	
@@ -131,5 +140,8 @@ func _on_sprite_animation_finished() -> void:
 	action = Action.NONE
 	sprite.play("idle")
 
-func _on_button_timer_timeout() -> void:
+func _on_hold_button_timer_timeout() -> void:
 	lay_egg()
+
+func _on_quick_press_button_timer_timeout() -> void:
+	button_charge = 0
