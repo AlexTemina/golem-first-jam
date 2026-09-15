8f5fd2537a5f69753e790dbac367e5080f181675
“Raul” <raulmj83@gmail.com>
2026-09-15 13:12:55 +0200

    show crow when bell sequence completed

---
+++ golem_first_jam/entities/npc/crow/crow.gd
@@ -0,0 +1,4 @@
+class_name Crow extends Node2D
+
+func _ready() -> void:
+	z_index = position.y
--- golem_first_jam/managers/test_farm_manager/test_farm.gd
+++ golem_first_jam/managers/test_farm_manager/test_farm.gd
@@ -2,8 +2,13 @@ extends Node2D
 
 @onready var fade_screen := $CanvasLayer/Fade
 
+@onready var crow := $Crow
+
 func _ready() -> void:
 	SignalBus.game_time_over.connect(restart)
+	SignalBus.bell_sequence_completed.connect(show_crow) # TODO Do this in the crow manager
+	
+	crow.hide()
 	
 func restart():
 	fade_screen.show()
@@ -13,3 +18,6 @@ func restart():
 
 func wait(seconds: float):
 	await get_tree().create_timer(seconds).timeout
+	
+func show_crow(bell_id: Bell.Id):
+	crow.show()
