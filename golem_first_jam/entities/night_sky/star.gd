76e2c640bf41773ab2f9b824efe8d9c42b0c61e5
“Raul” <raulmj83@gmail.com>
2026-09-15 17:39:49 +0200

    add night sky scene with one hint

---
+++ golem_first_jam/entities/night_sky/star.gd
@@ -0,0 +1,40 @@
+class_name Star extends Node2D
+
+enum State {MOVING, ON_TARGET}
+
+## Speed in px/s
+@export var speed: float = 150.0
+## How random is the target position, in pixels
+@export var target_point_randomness: float = 3.0
+
+@onready var sprite := $Sprite
+
+var state := State.MOVING
+var target_position: Vector2
+var sprite_color: Color
+var t: float
+var twinkle_speed: float
+
+func init(target_position: Vector2):
+	self.target_position = target_position
+	var r = target_point_randomness
+	self.target_position += Vector2(randf_range(-r, r), randf_range(-r, r))
+	var starting_x = randi_range(-ScreenProps.WIDTH, ScreenProps.WIDTH * 2)
+	var starting_y = randi_range(-ScreenProps.HEIGHT, ScreenProps.HEIGHT * 2)
+	position = Vector2(starting_x, starting_y)
+	speed = randf_range(speed * 0.8, speed * 1.5)
+	sprite_color = Color.WHITE.darkened(randfn(0, 0.1))
+	t = 0.0
+	twinkle_speed = randf_range(10, 50)
+	
+func _process(delta: float) -> void:
+	var calculated_speed = speed
+	var target_distance = position.distance_to(target_position)
+	if target_distance < 100.0:
+		calculated_speed = speed * target_distance / 100.0
+	var next_position = position.move_toward(target_position, delta * calculated_speed)	
+	position = next_position
+	t += delta
+	var sprite_alpha = 1.0 - (sin(t * twinkle_speed) / 5.0)
+	sprite.modulate = sprite_color
+	sprite.modulate.a = sprite_alpha
---
+++ golem_first_jam/managers/night_sky_manager/night_sky_manager.gd
@@ -0,0 +1,38 @@
+class_name NightSky extends Node2D
+
+@export var star_scene: PackedScene = load("res://golem_first_jam/entities/night_sky/star.tscn")
+
+@onready var stars_container := $Stars
+@onready var vision_pictogram := $VisionPictogram
+
+func _ready() -> void:
+	clear_stars()
+	vision_pictogram.hide()
+	
+	draw_pictogram(vision_pictogram)
+	
+func draw_pictogram(pictogram: Polygon2D):
+	var previous_point: Vector2
+	for point in pictogram.polygon:
+		add_star(point)
+		if previous_point != null and previous_point != Vector2.ZERO:
+			add_interpolated_stars(previous_point, point)
+		previous_point = point
+		
+func add_interpolated_stars(point1: Vector2, point2: Vector2):
+	var stars_number: int = point1.distance_to(point2) / 10
+	print(str(point1) + ' to ' + str(point2))
+	for i in range(stars_number):
+		var lerp_factor = (i + 1.0) / float(stars_number)
+		var star_position = lerp(point1, point2, lerp_factor)
+		print(star_position)
+		add_star(star_position)
+		
+func add_star(target_position: Vector2):
+	var star = star_scene.instantiate()
+	stars_container.add_child(star)
+	star.init(target_position)
+
+func clear_stars():
+	for star in stars_container.get_children():
+		star.queue_free()
