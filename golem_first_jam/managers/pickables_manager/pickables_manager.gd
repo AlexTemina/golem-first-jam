0f709af53f55e4408adeccd3cbd4f875542c5b72
“Raul” <raulmj83@gmail.com>
2026-09-18 18:55:51 +0200

    Drop items

--- golem_first_jam/managers/pickables_manager/pickables_manager.gd
+++ golem_first_jam/managers/pickables_manager/pickables_manager.gd
@@ -2,15 +2,28 @@ class_name PickablesManager extends Node
 
 @export var pickables_container: Node2D
 
+var picked_entity: PickableEntity
+
 func _ready() -> void:
 	SignalBus.chicken_pecks.connect(on_chicken_pecks)
 	
 func on_chicken_pecks(chicken: Chicken):
-	for child in pickables_container.get_children():
-		if child is PickableEntity:
-			var pickable_entity: PickableEntity = child
-			print('chicken - pickable: ' + str(chicken.position) + ' ' + str(pickable_entity.position))
-			if pickable_entity.is_near_character_position(chicken.position):
-				pickable_entity.picked = true
-				pickable_entity.position = chicken.position + chicken.beak.position
-				pickable_entity.reparent(chicken.beak)
+	if picked_entity != null:
+		drop_item()
+	else:
+		for child in pickables_container.get_children():
+			if child is PickableEntity:
+				var pickable_entity: PickableEntity = child
+				if pickable_entity.is_near_character_position(chicken.position):
+					pick_up_item(chicken, pickable_entity)
+					
+func pick_up_item(chicken: Chicken, item: PickableEntity):
+	picked_entity = item
+	item.picked = true
+	item.position = chicken.position + chicken.beak.position
+	item.reparent(chicken.beak)
+
+func drop_item():
+	picked_entity.reparent(pickables_container)
+	picked_entity.position.y += 14 # Fall from beak
+	picked_entity = null
