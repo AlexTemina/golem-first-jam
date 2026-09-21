e72b98c92c32f5333da3b71b3b795e91d108ae13
“Raul” <raulmj83@gmail.com>
2026-09-21 13:58:53 +0200

    add chicken coup building

---
+++ golem_first_jam/entities/buildings/chicken_coup.gd
@@ -0,0 +1,34 @@
+@tool
+class_name ChickenCoup extends Node2D
+
+@export var roof_visible := true:
+	set(value):
+		roof_visible = value
+		_apply_roof_visibility()
+
+@onready var interior_sprite := $InteriorSprite
+@onready var roof := $ExteriorSprite
+@onready var animator := $Animator
+
+func _ready() -> void:
+	_apply_roof_visibility()	
+	interior_sprite.z_index = interior_sprite.position.y
+
+func _on_interior_body_entered(body: Node2D) -> void:
+	if body is Chicken:
+		show_roof(false)
+
+func _on_interior_body_exited(body: Node2D) -> void:
+	if body is Chicken:
+		show_roof()
+
+func show_roof(on := true):
+	roof_visible = on
+	animator.play("show_roof" if on else "hide_roof")
+
+func _apply_roof_visibility() -> void:
+	var sprite := get_node_or_null(^"ExteriorSprite") as Sprite2D
+	if sprite == null:
+		return
+	# sprite.visible = roof_visible
+	sprite.modulate.a = 1.0 if roof_visible else 0.0
