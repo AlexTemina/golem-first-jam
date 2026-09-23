4294fc1b67fa04b359ce31285c926a082d5bfdf5
“Raul” <raulmj83@gmail.com>
2026-09-23 17:32:45 +0200

    Fix npcs and chicken z-index

--- golem_first_jam/entities/npc/crow/crow.gd
+++ golem_first_jam/entities/npc/crow/crow.gd
@@ -27,13 +27,11 @@ var action := Action.IDLE
 var flight_target: Vector2
 
 func _ready() -> void:		
-	z_index = position.y
-	starting_position = self.position
+	starting_position = self.global_position
 		
 func _process(delta: float) -> void:
 	if is_flying():
 		position = position.move_toward(flight_target, flying_speed * delta)
-		z_index = position.y
 		if position.distance_to(flight_target) < 8:
 			if flight_target == starting_position:
 				set_action(Action.IDLE)
--- golem_first_jam/entities/npc/npc.gd
+++ golem_first_jam/entities/npc/npc.gd
@@ -2,4 +2,4 @@ class_name Npc extends CharacterBody2D
 
 func init():
 	SignalBus.register_npc.emit(self)
-	z_index = global_position.y
+	# z_index = global_position.y
