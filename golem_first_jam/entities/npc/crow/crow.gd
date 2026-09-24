6556250316ac70c07614adc2ce4eb4ed8584d12c
“Raul” <raulmj83@gmail.com>
2026-09-24 12:44:32 +0200

    Fix more offsets

--- golem_first_jam/entities/npc/crow/crow.gd
+++ golem_first_jam/entities/npc/crow/crow.gd
@@ -30,8 +30,11 @@ func _ready() -> void:
 	starting_position = self.global_position
 		
 func _process(delta: float) -> void:
-	if is_flying():
+	if is_flying():		
 		position = position.move_toward(flight_target, flying_speed * delta)
+		## TODO Use this code but finding a path somehow
+		# velocity = position.direction_to(flight_target) * flying_speed
+		# move_and_slide()
 		if position.distance_to(flight_target) < 8:
 			if flight_target == starting_position:
 				set_action(Action.IDLE)
