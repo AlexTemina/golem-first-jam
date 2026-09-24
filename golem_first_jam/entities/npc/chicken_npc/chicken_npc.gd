d8bf9d0080148e40b548fccbdb4a8e3bf513ccd2
“Raul” <raulmj83@gmail.com>
2026-09-24 19:54:27 +0200

    Make the chicken NPC flies

--- golem_first_jam/entities/npc/chicken_npc/chicken_npc.gd
+++ golem_first_jam/entities/npc/chicken_npc/chicken_npc.gd
@@ -1,13 +1,19 @@
 class_name ChickenNpc extends Npc
 
-enum Action {NONE, PECKING, FLYING, LAYING_EGG, ECSTATIC}
+enum Action {NONE, PECKING, FLYING, LAYING_EGG, ECSTATIC, WALKING, RUNNING}
 
 const ANIMATIONS = {
 	Action.NONE: "idle_with_legs",
+	Action.WALKING: "move_with_legs",
+	Action.RUNNING: "move_with_legs",
 	Action.PECKING: "peck_with_legs",
 	Action.FLYING: "fly",
 	Action.LAYING_EGG: "lay_egg_short",
 	Action.ECSTATIC: "ecstatic",
+}
+
+var CHOREOGRAPHIES = {
+	Action.FLYING: [Action.WALKING, Action.RUNNING, Action.FLYING]
 }
 
 ## Which action is capable of performing
@@ -15,15 +21,17 @@ const ANIMATIONS = {
 ## Faces right (true) or left (false)
 @export var faces_right: bool
 ## In px/s
+@export var move_speed: int = 100
+## In px/s
 @export var run_speed: int = 150
-## How many times you have to push during the quick press timer to fly (see time_to_quick_push)
-@export var pushes_to_fly: int = 3
 ## Vertical impulse for flying (px/s)
 @export var jump_max_impulse: float = 5.0
 ## Gravity modifier, the higher the more gravity
 @export var gravity_factor: float = 0.02
 ## Max height flying
 @export var max_height: float = 20.0
+## Time between actions
+@export var time_between_actions: float = 4.0
 
 @onready var body := $Body
 @onready var sprite := $Body/Sprite
@@ -34,11 +42,12 @@ const ANIMATIONS = {
 
 var z_velocity: float # For jumping/flying
 var z_offset: float # Distance from the floor when flying
-
 var action: Action
+var choreography: Array = []
 
 func _ready() -> void:
 	face_right(faces_right)
+	next_action_timer.wait_time = time_between_actions
 	
 func _physics_process(delta: float) -> void:
 	manage_velocity(delta)
@@ -50,7 +59,6 @@ func manage_velocity(delta: float):
 		z_velocity += get_gravity().y * gravity_factor * delta
 		z_offset += z_velocity
 		z_offset = max(-max_height, z_offset) # Max height is negative
-		print(z_offset)
 		if z_offset > 0.0:
 			land()
 		body.position.y = z_offset
@@ -66,17 +74,30 @@ func face_right(right := true):
 	body.scale.x = 1 if right else -1
 	
 func do_main_action():
-	set_action(main_action)	
-	match main_action:
-		Action.FLYING:
-			fly()
+	var next_action = main_action
+	if not choreography.is_empty():
+		next_action = choreography.pop_front()
+	elif main_action in CHOREOGRAPHIES.keys():		
+		choreography.append_array(CHOREOGRAPHIES.get(main_action))
+		next_action = choreography.pop_front()
+		turn()
+		
+	set_action(next_action)	
 
 func set_action(new_action: Action):
 	action = new_action
 	var animation = ANIMATIONS.get(action)
 	if animation != null:
 		sprite.play(animation)
-	eyes.visible = is_ecstatic()
+	eyes.visible = is_ecstatic()	
+	match action:
+		Action.WALKING:
+			move()
+		Action.RUNNING:
+			move(true)
+		Action.FLYING:
+			fly()
+	next_action_timer.start()
 
 func _on_sprite_animation_finished() -> void:
 	match action:
@@ -85,13 +106,17 @@ func _on_sprite_animation_finished() -> void:
 	set_action(Action.NONE)
 	next_action_timer.start()
 	
+func move(run := false):
+	velocity.x = run_speed if run else move_speed
+	if not faces_right:
+		velocity.x *= -1
+	
 func lay_egg():
 	if visible_on_screen.is_on_screen():
 		SignalBus.npc_lay_egg.emit(global_position)			
 		SignalBus.play_chicken_sound.emit(SoundManager.ChickenSound.LAY_EGG)
 		
 func fly() -> void:
-	set_action(Action.FLYING)
 	wings.show()
 	wings.play("default")
 	z_velocity = -jump_max_impulse
@@ -100,9 +125,13 @@ func fly() -> void:
 func land():
 	z_offset = 0.0
 	body.position.y = z_offset
+	velocity = Vector2.ZERO
 	wings.hide()
 	set_action(Action.NONE)
 	next_action_timer.start()
+	
+func turn():
+	faces_right = not faces_right
 	
 func is_moving() -> bool: return velocity != Vector2.ZERO	
 func is_idle() -> bool: return Action.NONE == action
@@ -110,4 +139,6 @@ func is_ecstatic() -> bool: return Action.ECSTATIC == action
 func is_flying() -> bool: return Action.FLYING == action
 
 func _on_next_action_timer_timeout() -> void:
+	if is_flying():
+		return # It will do something when lands
 	do_main_action()
