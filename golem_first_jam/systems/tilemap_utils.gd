01340aaf6aecb3910979395cc393d054c6651ef0
alext <alextemina@gmail.com>
2026-09-24 22:24:04 +0200

    fix tilemap

--- golem_first_jam/systems/tilemap_utils.gd
+++ golem_first_jam/systems/tilemap_utils.gd
@@ -1,8 +1,8 @@
 class_name TileMapUtils extends Node
 
-const TILE_SIZE = 32
 const NO_TILE = Vector2i(-1, -1)
 
 static func get_atlas_at_position(tilemap: TileMapLayer, position: Vector2):
-	var atlas_coords = tilemap.get_cell_atlas_coords(position / TILE_SIZE)
+	var cell = tilemap.local_to_map(tilemap.to_local(position))
+	var atlas_coords = tilemap.get_cell_atlas_coords(cell)
 	return atlas_coords if atlas_coords != NO_TILE else null
