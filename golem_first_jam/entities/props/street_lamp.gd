331992f3c63d2b94eda4a16144667342f5c3b449
“Raul” <raulmj83@gmail.com>
2026-09-25 12:03:56 +0200

    Add street light sprites

--- golem_first_jam/entities/props/street_lamp.gd
+++ golem_first_jam/entities/props/street_lamp.gd
@@ -1,6 +1,13 @@
 @tool
 class_name StreetLamp extends StaticBody2D
 
+enum SpriteType {WOOD_1, METAL, WOOD_2}
+
+## Sprite for the lamp
+@export var sprite_type: SpriteType:
+	set(value):
+		sprite_type = value
+		update_sprite()
 ## Radius of the light
 @export var radius: float = 2.0:
 	set(value):
@@ -8,6 +15,12 @@ class_name StreetLamp extends StaticBody2D
 		light.texture_scale = radius
 
 @onready var light := $PointLight2D
+@onready var sprite := $Sprite
 
 func _ready() -> void:
 	light.texture_scale = radius
+	update_sprite()
+	
+func update_sprite():
+	if sprite:
+		sprite.frame = sprite_type
